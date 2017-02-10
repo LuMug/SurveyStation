a871826dd87df1fbbcd90be65f80cc9a546ed001
diSumma <riccardo.disumma@samtrevano.ch>
2017-02-10 14:37:12 +0100

    Add files via upload

---
+++ Cartella di trasporto - Varie/form_user.php
@@ -0,0 +1,35 @@
+<form id="form-login">
+    <div class="form-group">
+        <label for="exampleInputEmail1">Email</label>
+        <input type="email" class="form-control m-t-xxs" id="exampleInputEmail1" placeholder="Enter email">
+    </div>
+    <div class="form-group">
+        <label for="exampleInputPassword1">Password</label>
+        <input type="password" name="password" class="password form-control m-t-xxs" id="exampleInputPassword1" placeholder="Password">
+    </div>
+    <div class="checkbox">
+        <label class="no-s">
+            <div class="checker"><span class="checked"><input type="checkbox"></span></div> Check me out
+        </label>
+    </div>
+    <button type="submit" class="btn btn-primary m-t-xs m-b-xs">Login</button>
+    <div class="">
+      Non hai un account?
+    </div>
+    <a class="btn btn-primary m-t-xs m-b-xs" id="btn-register">Registrati</a>
+</form>
+<form id="form-register" class="displayNo">
+    <div class="form-group">
+        <label for="exampleInputEmail1">Email address</label>
+        <input type="email" class="form-control m-t-xxs" id="exampleInputEmail1" placeholder="Enter email">
+    </div>
+    <div class="form-group">
+        <label for="exampleInputPassword1">Password</label>
+        <input type="password" name="password" class="password form-control m-t-xxs" id="password_register" placeholder="Password">
+    </div>
+    <button type="submit" class="btn btn-primary m-t-xs m-b-xs">Registrati</button>
+    <div class="">
+      Hai un account?
+    </div>
+    <a class="btn btn-primary m-t-xs m-b-xs" id="btn-login">Accedi</a>
+</form>
