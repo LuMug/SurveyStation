bd5bb36c8c7f68dae7fa3cd4c98ef95e5d871bef
JonFas <jonathan.fassora@samtrevano.ch>
2017-02-24 13:59:35 +0100

    Update tabella_utenti.sql

--- database/tabella_utenti.sql
+++ database/tabella_utenti.sql
@@ -1,7 +1,7 @@
 create table utenti(
   `ID_Utente` int(11) AUTO_INCREMENT,
   `Email` varchar(50) NOT NULL,
-  `Password` binary(32) NOT NULL,
-  `Amministratore` tinyint(4) NOT NULL,
+  `Password` varchar(255) NOT NULL,
+  `Amministratore` tinyint(1) NOT NULL,
   PRIMARY KEY (`ID_Utente`)
 );
