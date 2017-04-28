85289e1874ac260274ec3153099ac19cc58908ad
JonFas <jonathan.fassora@samtrevano.ch>
2017-04-28 15:17:43 +0200

    Add files via upload

--- ArduinoCode/index.php
+++ ArduinoCode/index.php
@@ -1,25 +1,26 @@
 <?php
 //controllo se sono presenti i parametri valore e localita
-if(isset($_GET['valore']))
+if(isset($_GET['x']) && isset($_GET['y']) && isset($_GET['z']))
 {
 //Recupero il valore del parametro "valore"
-$valore = $_GET['valore'];
-echo $valore;
+$x = $_GET['x'];
+$y = $_GET['y'];
+$z = $_GET['z'];
  
 //eseguo la connessione al database sul server locale
 //inserendo nome utente e password
-$link = mysql_connect('10.20.4.126', 'root', 'root');
+$link = mysql_connect('localhost', 'root', 'root');
  
 //gestione degli errori
 if (!$link) {die('Impossibile connettersi: ' . mysql_error());}
  
 //seleziono il databse di nome arduino
-mysql_select_db("efof_samtinfoch43") or die( "Impossibile selezionare il database.");
+mysql_select_db("surveystation") or die( "Impossibile selezionare il database.");
  
 //creo una stringa sql di inserimento con i valori
 //recuperati dall'url
-$sql = "INSERT INTO efof_samtinfoch43.valuelog (value) 
-		VALUES (".$valore.")"; 
+$sql = "INSERT INTO surveystation.sismografo (Valore_X, Valore_Y, Valore_Z, Data) 
+		VALUES (".$x.", ".$y.", ".$z.", now())"; 
  
 //eseguo la query
 $retval = mysql_query( $sql, $link );
