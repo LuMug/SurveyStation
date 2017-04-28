ee4b2267548f9edc080c5e5c756ed2146b9142ef
Jacopogreppi <jacopo.greppi@samtrevano.ch>
2017-04-28 12:02:10 +0200

    Update database.sql

--- database/database.sql
+++ database/database.sql
@@ -1,6 +1,6 @@
-drop database if exists surveyStation;
-create database surveystation;
-use surveystation;
+drop database if exists svProva;
+create database svProva;
+use svProva;
 
 create table utenti(
   `Email` varchar(60) NOT NULL unique,
@@ -18,8 +18,6 @@ create table configurazione(
   PRIMARY KEY (`ID`)
 );
 
-#select * from configurazione;
-#delete from configurazione;
 insert into configurazione(Parametro,Valore) values('tempoPrimaPiccoDati',15);
 set @a = (select valore from configurazione where Parametro = 'tempoPrimaPiccoDati');
 
@@ -32,27 +30,11 @@ set @c = (select valore from configurazione where Parametro = 'CambioNumeroTerre
 insert into configurazione(Parametro,Valore) values('CancellareDatiVecchi',1);
 set @d = (select valore from configurazione where Parametro = 'CancellareDatiVecchi');
 
-#create table vibrazione_MEM(
-#  `ID` int(11) AUTO_INCREMENT not null,
-#  `Data` datetime NOT NULL default now(),
- # `Val_Vibrazione_MEM` double NOT NULL,
- # PRIMARY KEY (`ID`)
-#)ENGINE = MEMORY;
-
-#create table vibrazione_FIX(
-#  `ID` int(11) not null,
-#  `ID_Vibrazione_MEM` int(11) not null,
-#  `Data` datetime NOT NULL default now(),
-#  `Val_Vibrazione_FIX` double NOT NULL,
-#  PRIMARY KEY AUTO_INCREMENT (`ID`)
-#)ENGINE = MEMORY;
-
-#alter table vibrazione_FIX add foreign key (ID_Vibrazione_MEM) REFERENCES vibrazione_MEM (ID) ON DELETE CASCADE;
 
 create table shake(
   `ID_Sismografo` int(11) not null,
   `ID` int(11) not null AUTO_INCREMENT,
-  `Data` datetime NOT NULL default now(),
+  `Data` datetime NOT NULL default 0,
   `Valore_X` double NOT NULL,
   `Valore_Y` double NOT NULL,
   `Valore_Z` double NOT NULL,
@@ -61,35 +43,17 @@ create table shake(
 
 create table sismografo  (
   `ID` int(11) AUTO_INCREMENT not null,
-  `Data` datetime NOT NULL default now(),
+  `Data` datetime NOT NULL default 0,
   `Valore_X` double NOT NULL,
   `Valore_Y` double NOT NULL,
   `Valore_Z` double NOT NULL,  
   PRIMARY KEY (`ID`) 
 )ENGINE=MEMORY;
 
-#drop table shake;
-
-#insert into sismografo (Valore_X, Valore_Y, Valore_Z) values (1, 1, 1);
-#insert into sismografo (Valore_X, Valore_Y, Valore_Z) values (1, 1, 2);
-#insert into sismografo (Valore_X, Valore_Y, Valore_Z) values (1, 2, 1);
-#insert into sismografo (Valore_X, Valore_Y, Valore_Z) values (1, 2, 2);
-#insert into sismografo (Valore_X, Valore_Y, Valore_Z) values (2, 2, 2);
-#insert into sismografo (Valore_X, Valore_Y, Valore_Z) values (4, 79, 48);
-#insert into sismografo (Valore_X, Valore_Y, Valore_Z) values (62, 62, 32);
-#insert into sismografo (Valore_X, Valore_Y, Valore_Z) values (79, 96, 2);
-#insert into sismografo (Valore_X, Valore_Y, Valore_Z) values (10, 61, 84);
-#insert into sismografo (Valore_X, Valore_Y, Valore_Z) values (16, 42, 92);
-#insert into sismografo (Valore_X, Valore_Y, Valore_Z) values (59, 100, 6);
-#delete from sismografo;
-
-#select * from shake;
-
-
 delimiter //
 CREATE PROCEDURE storePreviousValues(IN idSism INT) #procedura per immagazzinare tutti i dati precedenti al valore che dà inizio al terremoto
 BEGIN #inizio a scrivere il codice della procedura
-	DECLARE X,Y,Z double; #variabili che conterranno i valori di Valore_X, Valore_Y, Valore_Z
+  DECLARE X,Y,Z double; #variabili che conterranno i valori di Valore_X, Valore_Y, Valore_Z
     DECLARE t datetime; #variabile che conterrà il valore di Data
     DECLARE done INT default FALSE; #variabile che serve per uscire da un loop
     #Seleziono i dati degli ultimi 15minuti nei quali non ci sia già stato registrato un picco di valore 
@@ -97,69 +61,59 @@ BEGIN #inizio a scrivere il codice della procedura
     DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE; #Quando arrivo alla fine dei records, esco dal ciclo
     
     OPEN cur;
-	read_loop: LOOP
-		FETCH cur INTO t, x,y,z; #assegno ogni valore dei record della tabella, alle rispettive variabili
-		IF done THEN
-			LEAVE read_loop; #se non ci sono più record, esco dal loop
-		END IF;
-	 
-		insert into shake(shake.ID, Data, Valore_X, Valore_Y, Valore_Z) values(idSism, t,x,y,z); #Inserisco i valori assegnati alle variabili del ciclo, nei rispettivi record della tabella shake
+  read_loop: LOOP
+    FETCH cur INTO t, x,y,z; #assegno ogni valore dei record della tabella, alle rispettive variabili
+    IF done THEN
+      LEAVE read_loop; #se non ci sono più record, esco dal loop
+    END IF;
+   
+    insert into shake(shake.ID, Data, Valore_X, Valore_Y, Valore_Z) values(idSism, t,x,y,z); #Inserisco i valori assegnati alle variabili del ciclo, nei rispettivi record della tabella shake
 
-		END LOOP;
-	CLOSE cur; 
-END #finisco di scrivere il codice della procedura
+    END LOOP;
+  CLOSE cur; 
+END; #finisco di scrivere il codice della procedura
 //
-delimiter;
-
-#drop procedure storePreviousValues;
-#drop trigger if exists InsertImportantDataFromSismografoToShake;
+delimiter ;
 
 delimiter // 
 
 create trigger InsertImportantDataFromSismografoToShake #Creo un trigger per poter inserire dei valori in una tabella a partire dall'individuamento di un determinato valore
-before insert ON surveystation.sismografo FOR EACH ROW #Operazione che va fatta, ovviamente, prima che un dato venga inserito
+before insert ON svProva.sismografo FOR EACH ROW #Operazione che va fatta, ovviamente, prima che un dato venga inserito
 BEGIN 
-	DECLARE sismografoId int default 0; #dichiaro una variabile che simboleggia il valore dell'id del sismografo e di defaul la metto a 0
+  DECLARE sismografoId int default 0; #dichiaro una variabile che simboleggia il valore dell'id del sismografo e di defaul la metto a 0
     DECLARE lastSismografoTime datetime; #dichiaro una variabile che simboleggia l'ultimo dato letto prima del picco che dà inizio al terremoto
     DECLARE saveData int default 0; #dichiaro una variaible nella quale salverò i dati precedenti al terremoto
-	SET sismografoId = (SELECT MAX(shake.ID) FROM shake); #il valore di sismografoId corrisponde al massimo valore del campo ID_Shake nella tabella shake
+  SET sismografoId = (SELECT MAX(shake.ID) FROM shake); #il valore di sismografoId corrisponde al massimo valore del campo ID_Shake nella tabella shake
     IF sismografoId is NULL THEN #se il campo sismografoId dovesse essere di tipo null allora lo si imposta a 0
-		 SET sismografoId=0;
+     SET sismografoId=0;
     END IF;
     SET lastSismografoTime = (SELECT MAX(Data) from shake); #il valore di lastSismografoTime corrisponde alla data più recente nella tabella shake
    
     if lastSismografoTime <= DATE_ADD(now(),INTERVAL -@c MINUTE) then #Se l'ultimo valore prima dell'inizio del terremoto è stato registrato più di un minuto fa, allora aumento il valore della variabile sismografoId che rappresenta il numero del terremoto
-		SET sismografoId = sismografoId +1;
+    SET sismografoId = sismografoId +1;
     end if;
-	
-	SET saveData = (select count(*) from shake where Data >= DATE_ADD(now(), INTERVAL - @b minute) and Valore_X > 3); #Definisco se salvare i dati solamente se negli ultimi 30 minuti c'è stato un picco
-	IF (new.Valore_X > 3) then #se il valore di Valore_X è paggiore di 3 allora chiamo la procedura storePreviousValues e le passo l'id del sismografo
-		CALL storePreviousValues(sismografoId);
+  
+  SET saveData = (select count(*) from shake where Data >= DATE_ADD(now(), INTERVAL - @b minute) and Valore_X > 3); #Definisco se salvare i dati solamente se negli ultimi 30 minuti c'è stato un picco
+  IF (new.Valore_X > 3) then #se il valore di Valore_X è paggiore di 3 allora chiamo la procedura storePreviousValues e le passo l'id del sismografo
+    CALL storePreviousValues(sismografoId);
     END IF;
-	
-	#Inserisco i dati nella tavella shake solamente se il dato è un picco, oppure se è meno vecchio di un minuto dall'ultimo dato registrato, oppure, se saveData>0 (spiegato sopra)
-	IF (new.Valore_X > 3) OR ((lastSismografoTime <= DATE_ADD(now(),INTERVAL -@c MINUTE)) OR (saveData > 0)) THEN
+  
+  #Inserisco i dati nella tavella shake solamente se il dato è un picco, oppure se è meno vecchio di un minuto dall'ultimo dato registrato, oppure, se saveData>0 (spiegato sopra)
+  IF (new.Valore_X > 3) OR ((lastSismografoTime <= DATE_ADD(now(),INTERVAL -@c MINUTE)) OR (saveData > 0)) THEN
         insert into shake (shake.ID,Valore_X,Valore_Y,Valore_Z) values(sismografoId,new.Valore_X,new.Valore_Y,new.Valore_Z); 
-	END IF;
+  END IF;
 END;
 
-// delimiter ;
-
-#select * from sismografo;
-#insert into sismografo values (null,now(),RAND(),1,1);
-#select * from sismografo;
-#delete from sismografo;
+// 
+delimiter ;
 
 delimiter //
 create procedure oldData()
 begin
-	delete from sismografo where data < now() - INTERVAL @d minute;
+  delete from sismografo where data < now() - INTERVAL @d minute;
 end;
 // delimiter ;
 
-#select * from sismografo;
-#drop procedure oldData;
-#call oldData();
 
 SET GLOBAL event_scheduler = ON;
 SET @@global.event_scheduler = ON;
@@ -167,8 +121,7 @@ SET GLOBAL event_scheduler = 1;
 SET @@global.event_scheduler = 1;
 
 create event runProcedureOldData
-	ON SCHEDULE EVERY 1 minute
+  ON SCHEDULE EVERY 1 minute
     do call oldData();
-    
-#drop event runProcedureOldData;
+
 
